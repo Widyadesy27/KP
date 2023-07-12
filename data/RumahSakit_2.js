d90f22e0f25721d4b5ea94dfd1d8b7b32c47adc1
Widyadesy27 <139054036+Widyadesy27@users.noreply.github.com>
2023-07-12 13:42:04 +0700

    Add files via upload

---
+++ data/RumahSakit_2.js
@@ -0,0 +1,9 @@
+var json_RumahSakit_2 = {
+"type": "FeatureCollection",
+"name": "RumahSakit_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Rumah Sakit Bethesda", "Koordinat X": 110.377696, "Koordinat Y": -7.783962, "Alamat": "Jl. Jend. Sudirman No.70, Kotabaru, Kec. Gondokusuman, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55224", "Deskripsi": "Rumah Sakit Bethesda: Pusat perawatan modern, tim medis berpengalaman, fasilitas canggih, perawatan holistik. Kebersihan, keamanan, dan kenyamanan menjadi prioritas utama.", "Foto": "E:\\Semester 6\\KP BAPPEDA\\Folder Foto Tempat Bersejarah\\Folder Foto Tempat Bersejarah\\Rumah Sakit Bethesda.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.377696, -7.783962 ] } },
+{ "type": "Feature", "properties": { "Nama": "Rumah Sakit Panti Rapih", "Koordinat X": 110.37619, "Koordinat Y": -7.777078, "Alamat": "Jl. Cik Di Tiro No.30, Samirono, Terban, Kec. Gondokusuman, Kota Yogyakarta, Daerah Istimewa Yogyakarta 55223", "Deskripsi": "Rumah Sakit Panti Rapih: Layanan medis terpercaya, keahlian medis terkini, dan perawatan holistik. Lingkungan yang nyaman, peralatan modern, dan tim perawatan yang penuh perhatian. Tempat penyembuhan yang dipercaya, harapan bagi kesembuhan dan kesejahteraan.", "Foto": "E:\\Semester 6\\KP BAPPEDA\\Folder Foto Tempat Bersejarah\\Folder Foto Tempat Bersejarah\\Rumah Sakit Panti Rapih.jpg" }, "geometry": { "type": "Point", "coordinates": [ 110.37619, -7.777078 ] } }
+]
+}
